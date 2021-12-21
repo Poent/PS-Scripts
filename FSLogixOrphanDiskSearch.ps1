6d629675874643c9783b9ac998e9433e9a6e65e6
Poent <Poent@users.noreply.github.com>
2021-12-21 16:08:13 -0700

    Update FSLogixOrphanDiskSearch.ps1

--- FSLogixOrphanDiskSearch.ps1
+++ FSLogixOrphanDiskSearch.ps1
@@ -1,20 +1,23 @@
-$group = Get-ADGroupMember -Identity CitrixCloud-RDS-Servers | foreach { $_.name }
+#This script will search the sessions hosts that are part of the "$SHGroupName" AD Group for attached FSLogix drives.
+#if you see the disk attached to a machine where the user does not have an existing session, then FSLogix has 
+#likely lost it's handle on the disk and it may need to be manually detached through diskmgr. 
+#rebooting the affected session host is the ideal solution to orphaned disks, but that may not always be possible during 
+#production.
+
+$SHGroupName = "CitrixCloud-RDS-Servers"
+
+$group = Get-ADGroupMember -Identity $SHGroupName | foreach { $_.name }
 $username = Read-Host -Prompt "Enter the username you're hunting for"
 $username = $username.ToLower()
+$creds = Get-Credential -Message "Login with account authorized for WinRM..."
 foreach ( $srv in $group ) {
     Write-Host "Scanning " $srv "..."
-    $disks = Invoke-Command $srv { get-disk }
+    $disks = Invoke-Command $srv { get-disk } -Credential ($creds)
     foreach ($disk in $disks){
         $location = $disk.Location
- #       
         if($location.ToLower().Contains($username)){
             write-host -ForegroundColor Green "Found profile on Server " $srv "..."
             write-host -ForegroundColor Green $location
         }
     }
-    
-   
- #  Invoke-Command $srv { 
- #      get-disk | Where-Object { $_.Location.ToLower().Contains($username) }
- #      }
-    }
+}
